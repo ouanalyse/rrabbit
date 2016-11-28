f8a1d76d5afffd29361634b1270d2c47091a28b6
Michal Bohuslávek <mbohuslavek@gmail.com>
2016-11-28 13:07:48 +0000

    Move the licensed code to the bottom in src/rabbit.cpp

--- src/rabbit.cpp
+++ src/rabbit.cpp
@@ -1,36 +1,3 @@
-/*
- * Version: MIT
- *
- * Portions created by Alan Antonuk are Copyright (c) 2012-2013
- * Alan Antonuk. All Rights Reserved.
- *
- * Portions created by VMware are Copyright (c) 2007-2012 VMware, Inc.
- * All Rights Reserved.
- *
- * Portions created by Tony Garnock-Jones are Copyright (c) 2009-2010
- * VMware, Inc. and Tony Garnock-Jones. All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -40,50 +7,6 @@
 
 #include <R.h>
 #include <Rcpp.h>
-
-void die_on_error(int x, char const *context) {
-	if (x < 0) {
-		Rcpp::stop("%s: %s", context, amqp_error_string2(x));
-	}
-}
-
-void die_on_amqp_error(amqp_rpc_reply_t x, char const *context) {
-	switch (x.reply_type) {
-	case AMQP_RESPONSE_NORMAL:
-		return;
-	case AMQP_RESPONSE_NONE:
-		Rcpp::stop("%s: missing RPC reply type!", context);
-		break;
-	case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-		Rcpp::stop("%s: %s", context, amqp_error_string2(x.library_error));
-		break;
-	case AMQP_RESPONSE_SERVER_EXCEPTION:
-		switch (x.reply.id) {
-		case AMQP_CONNECTION_CLOSE_METHOD: {
-			amqp_connection_close_t *m = (amqp_connection_close_t *) x.reply.decoded;
-			Rcpp::stop("%s: server connection error %uh, message: %.*s",
-				context,
-				m->reply_code,
-				(int) m->reply_text.len, (char *) m->reply_text.bytes);
-			break;
-		}
-		case AMQP_CHANNEL_CLOSE_METHOD: {
-			amqp_channel_close_t *m = (amqp_channel_close_t *) x.reply.decoded;
-			Rcpp::stop("%s: server channel error %uh, message: %.*s",
-				context,
-				m->reply_code,
-				(int) m->reply_text.len, (char *) m->reply_text.bytes);
-			break;
-		}
-		default:
-			Rcpp::stop("%s: unknown server error, method id 0x%08X", context, x.reply.id);
-			break;
-		}
-		break;
-	}
-	fprintf(stderr, "rabbitmq-c: unknown error");
-	exit(1);
-}
 
 // [[Rcpp::export]]
 Rcpp::XPtr<amqp_connection_state_t_> open_conn(std::string hostname, int port, std::string username, std::string password) {
@@ -106,7 +29,7 @@ Rcpp::XPtr<amqp_connection_state_t_> open_conn(std::string hostname, int port, s
 	die_on_amqp_error(amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, username.c_str(), username.c_str()), "logging in");
 
 	// Note that amqp_connection_state_t is really amqp_connection_state_t_*. As the Rcpp::XPtr
-	// template needs a pointer, cast (amqp_connection_state_t( to (amqp_connection_state_t_*).
+	// template needs a pointer, cast (amqp_connection_state_t) to (amqp_connection_state_t_*).
 	return Rcpp::XPtr<amqp_connection_state_t_>((amqp_connection_state_t_*) conn, true);
 }
 
@@ -212,4 +135,87 @@ std::string consume_message(Rcpp::XPtr<amqp_connection_state_t_> conn) {
 	std::string msg ((char *) envelope.message.body.bytes, envelope.message.body.len);
 	amqp_destroy_envelope(&envelope);
 	return msg;
+}
+
+void die_on_error(int x, char const *context) {
+	if (x < 0) {
+		Rcpp::stop("%s: %s", context, amqp_error_string2(x));
+	}
+}
+
+/*
+ * The following function is derived from RabbitMQ C AMQP client library
+ * examples/utils.c
+ *
+ * https://github.com/alanxz/rabbitmq-c/blob/a45fe6f30ba0bc24da0c6c46ff3c2dc270cf2b6d/examples/utils.c
+ *
+ *
+ * Version: MIT
+ *
+ * Portions created by Alan Antonuk are Copyright (c) 2012-2013
+ * Alan Antonuk. All Rights Reserved.
+ *
+ * Portions created by VMware are Copyright (c) 2007-2012 VMware, Inc.
+ * All Rights Reserved.
+ *
+ * Portions created by Tony Garnock-Jones are Copyright (c) 2009-2010
+ * VMware, Inc. and Tony Garnock-Jones. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+void die_on_amqp_error(amqp_rpc_reply_t x, char const *context) {
+	switch (x.reply_type) {
+	case AMQP_RESPONSE_NORMAL:
+		return;
+	case AMQP_RESPONSE_NONE:
+		Rcpp::stop("%s: missing RPC reply type!", context);
+		break;
+	case AMQP_RESPONSE_LIBRARY_EXCEPTION:
+		Rcpp::stop("%s: %s", context, amqp_error_string2(x.library_error));
+		break;
+	case AMQP_RESPONSE_SERVER_EXCEPTION:
+		switch (x.reply.id) {
+		case AMQP_CONNECTION_CLOSE_METHOD: {
+			amqp_connection_close_t *m = (amqp_connection_close_t *) x.reply.decoded;
+			Rcpp::stop("%s: server connection error %uh, message: %.*s",
+				context,
+				m->reply_code,
+				(int) m->reply_text.len, (char *) m->reply_text.bytes);
+			break;
+		}
+		case AMQP_CHANNEL_CLOSE_METHOD: {
+			amqp_channel_close_t *m = (amqp_channel_close_t *) x.reply.decoded;
+			Rcpp::stop("%s: server channel error %uh, message: %.*s",
+				context,
+				m->reply_code,
+				(int) m->reply_text.len, (char *) m->reply_text.bytes);
+			break;
+		}
+		default:
+			Rcpp::stop("%s: unknown server error, method id 0x%08X", context, x.reply.id);
+			break;
+		}
+		break;
+	}
+	fprintf(stderr, "rabbitmq-c: unknown error");
+	exit(1);
 }
